7dccfd3263039cc163bd7875533d5ed68bdbb35d
Diana Islas Ocampo <dev@hyuchia.com>
2023-12-13 13:45:26 -0600

    Add missing export

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Dream = exports.ChatGPT = void 0;
+exports.Gemini = exports.Dream = exports.ChatGPT = void 0;
 var openai_1 = require("./openai");
 Object.defineProperty(exports, "ChatGPT", { enumerable: true, get: function () { return __importDefault(openai_1).default; } });
 var stability_1 = require("./stability");
 Object.defineProperty(exports, "Dream", { enumerable: true, get: function () { return __importDefault(stability_1).default; } });
+var google_1 = require("./google");
+Object.defineProperty(exports, "Gemini", { enumerable: true, get: function () { return __importDefault(google_1).default; } });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as ChatGPT } from './openai';
 export { default as Dream, Style, Size, DreamResponse } from './stability';
+export { default as Gemini } from './google';

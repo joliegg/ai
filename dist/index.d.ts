55937b90f547a71088d7fd287b52e2865023602f
Diana Islas Ocampo <dev@hyuchia.com>
2025-07-21 21:25:16 -0600

    Fix class rename

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 import ChatGPT from './openai';
 import Dream from './stability';
 import DeepSeek from './deepseek';
-import Google from './google';
+import Gemini from './gemini';
 import Grok from './grok';
-export { ChatGPT, Dream, DeepSeek, Google, Grok };
+export { ChatGPT, Dream, DeepSeek, Gemini, Grok };
--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Grok = exports.Google = exports.DeepSeek = exports.Dream = exports.ChatGPT = void 0;
+exports.Grok = exports.Gemini = exports.DeepSeek = exports.Dream = exports.ChatGPT = void 0;
 const openai_1 = __importDefault(require("./openai"));
 exports.ChatGPT = openai_1.default;
 const stability_1 = __importDefault(require("./stability"));
 exports.Dream = stability_1.default;
 const deepseek_1 = __importDefault(require("./deepseek"));
 exports.DeepSeek = deepseek_1.default;
-const google_1 = __importDefault(require("./google"));
-exports.Google = google_1.default;
+const gemini_1 = __importDefault(require("./gemini"));
+exports.Gemini = gemini_1.default;
 const grok_1 = __importDefault(require("./grok"));
 exports.Grok = grok_1.default;
